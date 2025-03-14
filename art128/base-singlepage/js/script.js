70ec0dbe287ae6f731a27b00f25fa5c2c3f952ef
AA-C-hub <aacc3@hawaii.edu>
2025-03-13 19:24:51 -1000

    singlepage

---
+++ art128/base-singlepage/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav');
+    // console.log(menubutton);
+    // console.log(menunav);
+    menubutton.addEventListener('click', function () {
+        if (menunav.getAttribute('data-navstate') === 'open') {
+            menunav.setAttribute('data-navstate', 'closed');
+        } else {
+            menunav.setAttribute('data-navstate', 'open');
+        };
+    });
+    
+});
